3731f17d1014d27fb7c75ea16d4841f612e4707c
Moviro <135271794+Moviro@users.noreply.github.com>
2023-06-15 16:45:18 -0300

    Add files via upload

---
+++ Challenge-encriptador/index.js
@@ -0,0 +1,47 @@
+function encriptar(){
+   let texto = document.getElementById("texto").value;
+   let tituloMensaje = document.getElementById("titulo-mensaje");
+   let parrafo = document.getElementById("parrafo");
+   let muñeco = document.getElementById("muñeco");
+   let textoCifrado = texto
+      .replace(/e/gi,"enter")
+      .replace(/i/gi,"imes")
+      .replace(/a/gi,"ai")
+      .replace(/o/gi,"ober")
+      .replace(/u/gi,"ufat");
+   if (texto.length != 0){
+     document.getElementById("texto").value = textoCifrado;
+     tituloMensaje.textContent = "texto encriptado con exito";
+     parrafo.textContent = "";
+   } else{
+      tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que desees encriptar o desencriptar";
+
+   }
+   
+   
+}
+
+function desencriptar(){
+   let texto = document.getElementById("texto").value;
+   let tituloMensaje = document.getElementById("titulo-mensaje");
+   let parrafo = document.getElementById("parrafo");
+   let muñeco = document.getElementById("muñeco");
+   let textoCifrado = texto
+      .replace(/enter/gi,"e")
+      .replace(/imes/gi,"i")
+      .replace(/ai/gi,"a")
+      .replace(/ober/gi,"o")
+      .replace(/ufat/gi,"u");
+   if (texto.length != 0){
+     document.getElementById("texto").value = textoCifrado;
+     tituloMensaje.textContent = "texto desencriptado con exito";
+     parrafo.textContent = "";
+   } else{
+      tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que desees encriptar o desencriptar";
+
+   }
+   
+   
+}
